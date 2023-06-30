fee8d42685f48298e1ba40db06b487b323c8b413
abalmasDA <abalmasit@gmail.com>
2023-06-30 18:59:48 +0300

    Commit1

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Car {
+public class Car implements Comparable<Car> {
+    private int speed;
+    private int price;
+    private String model;
+    private String color;
+
+    public Car(int speed, int price, String model, String color) {
+        this.speed = speed;
+        this.price = price;
+        this.model = model;
+        this.color = color;
+    }
+
+    public String toString() {
+        return this.model + " " + this.color + " " + this.speed + " " + this.price;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+    @Override
+    public int compareTo(Car car) {
+        int sortBySpeed = this.speed - car.getSpeed();
+        int sortByPrice = this.price - car.getPrice();
+        int sortByModel = this.model.compareTo(car.getModel());
+        int sortByColor = this.color.compareTo(car.getColor());
+
+        if (sortBySpeed != 0) {
+            return sortBySpeed;
+        } else if (sortByPrice != 0) {
+            return sortByPrice;
+        } else if (sortByModel != 0) {
+            return sortByModel;
+        } else {
+            return sortByColor;
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+
+/**
+ * У папці з прикладами ex_004_comparable. Дописати логіку,
+ * щоб метод compareTo() здійснив пошук за швидкістю
+ * (якщо ж швидкість у 2-х об'єктів дорівнює, то шукаємо за ціною) -> ціною -> моделі -> кольору машини.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car[] carArray = new Car[]{
+                new Car(10, 10000, "Kia", "Green"),
+                new Car(150, 10000, "BMW", "Green"),
+                new Car(80, 5000, "Kia", "Red"),
+                new Car(80, 5000, "Kia", "Black"),
+                new Car(160, 8000, "Volvo", "Black"),
+
+        };
+
+        System.out.println(Arrays.toString(carArray));
+        Arrays.sort(carArray);
+        System.out.println(Arrays.toString(carArray));
+
+    }
+
 }
